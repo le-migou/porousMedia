33349a906803c3f33c91695ee7d064aa7c17c26b
Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>
2024-11-07 14:20:36 +0100

    Cleanup erratum: forgot to format one file.
    Signed-off-by: Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>

--- libraries/porousMedia/geochemistry/geochemistryNone/geochemistryNone.C
+++ libraries/porousMedia/geochemistry/geochemistryNone/geochemistryNone.C
@@ -12,43 +12,32 @@ geochemistryModels
 defineTypeNameAndDebug (geochemistryNone, 0);
 addToRunTimeSelectionTable (geochemistryModel, geochemistryNone, dictionary);
 
-geochemistryNone::geochemistryNone (
-    fvMesh const& mesh,
-    porousMedia& parent
-)
-: 
-    geochemistryModel (
-        mesh, 
-        parent
-    ), 
-    coeffDict_ (parent.dict ().subDict (type () + "Coeffs")), 
-    eps0_ ("eps0", dimless, coeffDict_), 
-    eps_
-    (
-      IOobject
-      (
-          "eps",
-          mesh.time ().name (),
-          mesh,
-          IOobject::NO_READ,
-          IOobject::NO_WRITE
-      ),
-      mesh,
-      eps0_
-    ),
-    massSourceTerm_
-    (
-      IOobject
-      (
-          "massSourceTerm",
-          mesh.time ().name (),
-          mesh,
-          IOobject::NO_READ,
-          IOobject::NO_WRITE
-      ),
-      mesh,
-      dimensionedScalar (dimless/dimTime, 0)
-    )
+geochemistryNone::geochemistryNone (fvMesh const& mesh, porousMedia& parent)
+    : geochemistryModel { mesh, parent }
+    , coeffDict_ { parent.dict ().subDict (type () + "Coeffs") }
+    , eps0_  { "eps0", dimless, coeffDict_ }
+    , eps_ {
+          IOobject {
+              "eps"
+            , mesh.time ().name ()
+            , mesh
+            , IOobject::NO_READ
+            , IOobject::NO_WRITE
+          }
+        , mesh
+        , eps0_
+      }
+    , massSourceTerm_ {
+          IOobject {
+              "massSourceTerm"
+            , mesh.time ().name ()
+            , mesh
+            , IOobject::NO_READ
+            , IOobject::NO_WRITE
+          }
+        , mesh
+        , dimensionedScalar { dimless/dimTime, 0 }
+    }
 {}
 
 } // namespace geochemistryModels
